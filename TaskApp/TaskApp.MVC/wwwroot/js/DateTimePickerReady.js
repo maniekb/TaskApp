8980cad044dae3bd9882e8842cd31cd15dc8faf9
Maciek <maciej.bolsega@gmail.com>
2020-06-26 16:19:34 +0200

    removed unnecessary libs

--- TaskApp/TaskApp.MVC/wwwroot/js/DateTimePickerReady.js
+++
@@ -1,7 +0,0 @@
-﻿$(function () {
-    $.datetimepicker.setDateFormatter('moment');
-    $(".datetimefield").datetimepicker({
-        format: 'MM/DD/YYYY hh:mm A',
-        formatTime: 'h:mm A'
-    });
-});
